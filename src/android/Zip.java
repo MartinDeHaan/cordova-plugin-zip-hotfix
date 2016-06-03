ce49f2fd62dceb30df4657db883ebfa4f9324e06
Martin de Haan <martin@lucrasoft.nl>
2016-06-03 11:24:08 +0200

    Escape \

--- src/android/Zip.java
+++ src/android/Zip.java
@@ -120,7 +120,7 @@ public class Zip extends CordovaPlugin {
             {
                 anyEntries = true;
                 String compressedName = ze.getName();
-                compressedName=compressedName.replace("\", "/");
+                compressedName=compressedName.replace("\\", "/");
 
                 if (ze.isDirectory()) {
                    File dir = new File(outputDirectory + compressedName);
